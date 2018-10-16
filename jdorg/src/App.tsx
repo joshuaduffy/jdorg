9669474de9f385d3f66c61daf68920017dbb549c
Joshua Duffy <joshua.duffy@emishealth.com>
2018-10-16 17:04:03 +0100

    To be announced landing page

--- jdorg/src/App.tsx
+++ jdorg/src/App.tsx
@@ -1,41 +1,15 @@
 import * as React from 'react';
-
-import { getTitleApi } from './Api';
-import Header from './Header';
+import fistSvg from './fist.svg';
 
 import './App.css';
 
-interface IAppState {
-  title: string;
-}
+const app = () => {
+  return (
+    <div className="flex-container">
+      <img className="image" src={fistSvg} alt="fist" />
+      <span className="title">JD</span>
+    </div>
+  );
+};
 
-class App extends React.Component<{}, IAppState> {
-  public constructor(props:any) {
-    super(props);
-    this.state = {
-      title: 'Loading',
-    };
-  }
-
-  public componentDidMount() {
-    getTitleApi().then(
-      (resp) => {
-        const title = resp.data.title;
-        this.setState({ title });
-      },
-    );
-  }
-
-  public render() {
-    return (
-      <div className="App">
-        <Header title={this.state.title} />
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+export default app;
