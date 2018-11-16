2a9c1f9f3b325099ac48e99817fd48b5fb4a3cbd
Joshua Duffy <joshua.duffy@emishealth.com>
2018-11-16 12:04:26 +0000

    Linting

--- jdorg/src/App.test.tsx
+++ jdorg/src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import reactDom from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  reactDom.render(<App />, div);
+  reactDom.unmountComponentAtNode(div);
 });
--- jdorg/src/serviceWorker.ts
+++ jdorg/src/serviceWorker.ts
@@ -16,8 +16,8 @@ const isLocalhost = Boolean(
     window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
     window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/,
+    ),
 );
 
 export function register(config:any) {
@@ -41,9 +41,10 @@ export function register(config:any) {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
+          // tslint:disable-next-line:no-console
           console.log(
             'This web app is being served cache-first by a service ' +
-              'worker. To learn more, visit http://bit.ly/CRA-PWA'
+              'worker. To learn more, visit http://bit.ly/CRA-PWA',
           );
         });
       } else {
@@ -57,7 +58,7 @@ export function register(config:any) {
 function registerValidSW(swUrl:any, config:any) {
   navigator.serviceWorker
     .register(swUrl)
-    .then(registration => {
+    .then((registration) => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
@@ -69,9 +70,10 @@ function registerValidSW(swUrl:any, config:any) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
+              // tslint:disable-next-line:no-console
               console.log(
                 'New content is available and will be used when all ' +
-                  'tabs for this page are closed. See http://bit.ly/CRA-PWA.'
+                  'tabs for this page are closed. See http://bit.ly/CRA-PWA.',
               );
 
               // Execute callback
@@ -82,6 +84,7 @@ function registerValidSW(swUrl:any, config:any) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
+              // tslint:disable-next-line:no-console
               console.log('Content is cached for offline use.');
 
               // Execute callback
@@ -93,7 +96,8 @@ function registerValidSW(swUrl:any, config:any) {
         };
       };
     })
-    .catch(error => {
+    .catch((error) => {
+      // tslint:disable-next-line:no-console
       console.error('Error during service worker registration:', error);
     });
 }
@@ -101,7 +105,7 @@ function registerValidSW(swUrl:any, config:any) {
 function checkValidServiceWorker(swUrl:any, config:any) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
-    .then(response => {
+    .then((response) => {
       // Ensure service worker exists, and that we really are getting a JS file.
       const contentType = response.headers.get('content-type');
       if (
@@ -109,7 +113,7 @@ function checkValidServiceWorker(swUrl:any, config:any) {
         (contentType != null && contentType.indexOf('javascript') === -1)
       ) {
         // No service worker found. Probably a different app. Reload the page.
-        navigator.serviceWorker.ready.then(registration => {
+        navigator.serviceWorker.ready.then((registration) => {
           registration.unregister().then(() => {
             window.location.reload();
           });
@@ -120,15 +124,16 @@ function checkValidServiceWorker(swUrl:any, config:any) {
       }
     })
     .catch(() => {
+      // tslint:disable-next-line:no-console
       console.log(
-        'No internet connection found. App is running in offline mode.'
+        'No internet connection found. App is running in offline mode.',
       );
     });
 }
 
 export function unregister() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(registration => {
+    navigator.serviceWorker.ready.then((registration) => {
       registration.unregister();
     });
   }
