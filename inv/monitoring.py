4b2bcb8a2262b31915301167fbd0026811914501
Joshua Duffy <joshua.duffy@emishealth.com>
2018-12-03 16:24:43 +0000

    Generate credentials file

--- inv/monitoring.py
+++ inv/monitoring.py
@@ -5,8 +5,22 @@ from .util import chdir
 WORKING_DIR = 'monitoring'
 
 
-@task
-def up(c):
+@task(help={
+    "access-key": "A valid AWS access key.",
+    "secret-key": "A valid AWS secret key."
+})
+def up(c, access_key, secret_key):
     """Builds, creates/re-creates and starts the grafana container."""
     with chdir(WORKING_DIR):
+        __create_credentials_file(access_key, secret_key)
         docker_compose('up', '--force-recreate', '--renew-anon-volumes')
+
+
+def __create_credentials_file(access_key, secret_key):
+    config = [
+        '[default]',
+        f'aws_access_key_id = {access_key}',
+        f'aws_secret_access_key = {secret_key}'
+    ]
+    with open('credentials', 'w') as creds:
+        creds.write('\n'.join(config))
