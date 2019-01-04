b9d2f00eab322c87d4d167db52a27dec62cada67
Joshua Duffy <joshua.duffy@emishealth.com>
2019-01-04 14:39:02 +0000

    Pass in admin user/password

--- inv/monitoring.py
+++ inv/monitoring.py
@@ -1,4 +1,4 @@
-from os import remove
+from os import environ, remove
 from invoke import task
 from .commands import docker_compose
 from .util import chdir
@@ -8,9 +8,11 @@ WORKING_DIR = 'monitoring'
 
 @task(help={
     "access-key": "A valid AWS access key.",
-    "secret-key": "A valid AWS secret key."
+    "secret-key": "A valid AWS secret key.",
+    "admin-user": "The name to give the admin user.",
+    "admin-password": "The password for the admin user."
 })
-def up(c, access_key=None, secret_key=None):
+def up(c, admin_user, admin_password, access_key=None, secret_key=None):
     """Builds, creates/re-creates and starts the grafana container."""
     with chdir(WORKING_DIR):
         if access_key and secret_key:
@@ -19,6 +21,12 @@ def up(c, access_key=None, secret_key=None):
             except:
                 pass
             __create_credentials_file(access_key, secret_key)
+
+        environ.update({
+            'ADMIN_USER': admin_user,
+            'ADMIN_PASSWORD': admin_password
+        })
+
         docker_compose('up', '--force-recreate', '--renew-anon-volumes')
 
 
