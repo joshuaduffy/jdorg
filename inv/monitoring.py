398bab4acaa54f41e3720ae4c8a3876a9cfbb586
Joshua Duffy <joshua.duffy@emishealth.com>
2019-02-11 16:36:44 +0000

    Compose not working as expected, probably me, but oh well.

--- inv/monitoring.py
+++ inv/monitoring.py
@@ -8,14 +8,10 @@ WORKING_DIR = 'monitoring'
 
 @task(help={
     "access-key": "A valid AWS access key.",
-    "secret-key": "A valid AWS secret key.",
-    "admin-user": "The name to give the admin user.",
-    "admin-password": "The password for the admin user."
+    "secret-key": "A valid AWS secret key."
 })
-def up(c, admin_user, admin_password, access_key=None, secret_key=None):
+def up(c, access_key=None, secret_key=None):
     """Builds, creates/re-creates and starts the grafana container."""
-    __update_env(admin_user, admin_password)
-
     with chdir(WORKING_DIR):
         if access_key and secret_key:
             try:
@@ -24,22 +20,20 @@ def up(c, admin_user, admin_password, access_key=None, secret_key=None):
                 pass
             __create_credentials_file(access_key, secret_key)
 
-        docker_compose('up', '--force-recreate', '--renew-anon-volumes')
+        docker('build', '-t', 'grafana', '.')
+        docker('run', '-p', '3000:3000', 'grafana')
 
 
 @task(help={
-    "admin-user": "The name to give the admin user.",
-    "admin-password": "The password for the admin user.",
     "profile": "A valid AWS profile."
 })
-def push(c, profile, admin_user, admin_password, aws_account_id='380760145297', aws_region='eu-west-1', ecr_repo_name='joshuaduffy-graf-ecr', tag='latest'):
+def push(c, profile, aws_account_id='380760145297', aws_region='eu-west-1', ecr_repo_name='joshuaduffy-graf-ecr', tag='latest'):
     """Build and push the container to ECR"""
     c.run(aws('ecr', 'get-login', '--registry-ids', aws_account_id, '--no-include-email', '--profile', profile).stdout)
 
-    __update_env(admin_user, admin_password)
-
     with chdir(WORKING_DIR):
-        docker_compose('push')
+        docker('build', '-t', f'{aws_account_id}.dkr.ecr.{aws_region}.amazonaws.com/{ecr_repo_name}:{tag}', '.')
+        docker('push', f'{aws_account_id}.dkr.ecr.{aws_region}.amazonaws.com/{ecr_repo_name}:{tag}')
 
 
 def __create_credentials_file(access_key, secret_key):
@@ -50,14 +44,3 @@ def __create_credentials_file(access_key, secret_key):
     ]
     with open('credentials', 'w') as creds:
         creds.write('\n'.join(config))
-
-
-def __update_env(admin_user, admin_password):
-    environ.update({
-        'ADMIN_USER': admin_user,
-        'ADMIN_PASSWORD': admin_password,
-        'AWS_ACCOUNT_ID': '380760145297',
-        'AWS_REGION': 'eu-west-1',
-        'DOCKER_REPO': 'joshuaduffy-graf-ecr',
-        'DOCKER_TAG': 'latest'
-    })
